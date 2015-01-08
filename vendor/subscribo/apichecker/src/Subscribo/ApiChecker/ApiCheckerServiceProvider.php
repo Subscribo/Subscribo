0442aa8fe3b454e719a7287f1b2984a6f56060e1
slavo <slavo@localhost>
2015-01-08 13:53:57 +0100

    Package subscribo/apichecker: getting uri from config file

--- vendor/subscribo/apichecker/src/Subscribo/ApiChecker/ApiCheckerServiceProvider.php
+++ vendor/subscribo/apichecker/src/Subscribo/ApiChecker/ApiCheckerServiceProvider.php
@@ -14,14 +14,19 @@ class ApiCheckerServiceProvider extends ServiceProvider {
     public function register()
     {
         $this->app->register('\\Subscribo\\ModelBase\\Integration\\Laravel\\ModelBaseServiceProvider');
-        $this->app->make('router')->get('checker', function() {
-            return $this->app->make('view')->make('apichecker::checker');
-        });
-
     }
 
     public function boot()
     {
         $this->package('subscribo/apichecker');
+        $configurationManager = $this->app->make('subscribo.config');
+        $configDirectory = dirname(dirname(dirname(__FILE__))).'/config';
+        $configurationManager->loadFileForPackage('apichecker', 'config', false, false, $configDirectory);
+        $configurationManager->loadFileForPackage('apichecker', 'config', false, true, true);
+        $uri = $configurationManager->getForPackage('apichecker', 'uri', 'checker');
+        $this->app->make('router')->get($uri, function() {
+            return $this->app->make('view')->make('apichecker::checker');
+        });
+
     }
 }
