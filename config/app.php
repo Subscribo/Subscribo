3f5c0b92ef5151068734643683c084b2ed529525
slavo <slavo@localhost>
2015-02-20 15:42:37 +0100

    Removing subscribo/config/packages/schemabuilder/schema.yml from repository (it is now copied from package ModelCore) using DevelopmentSeeder: composer.json and config/app.php update project composer.json - removing some (direct) dependencies

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
         '\\Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+        '\\Subscribo\\DevelopmentSeeder\\Integration\\Laravel\\DevelopmentSeederServiceProvider',
 
 		/*
 		 * Application Service Providers...
