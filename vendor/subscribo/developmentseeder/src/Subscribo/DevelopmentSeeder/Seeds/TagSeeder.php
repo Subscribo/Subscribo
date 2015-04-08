e3772c4af648cf98f1ff4b890ecd14f2d954a37a
slavo <slavo@localhost>
2015-04-08 14:57:06 +0200

    Package Subscribo DevelopmentSeeder update  - TagSeeder localization

--- vendor/subscribo/developmentseeder/src/Subscribo/DevelopmentSeeder/Seeds/TagSeeder.php
+++ vendor/subscribo/developmentseeder/src/Subscribo/DevelopmentSeeder/Seeds/TagSeeder.php
@@ -8,17 +8,22 @@ class TagSeeder extends Seeder {
 
     public function run()
     {
-        $generalTagGroup = TagGroup::firstOrCreate(['identifier' => 'GENERAL', 'name' => 'General']);
+        $generalTagGroup = TagGroup::firstOrCreate(['identifier' => 'GENERAL']);
+        $generalTagGroup->name = 'General';
+        $generalTagGroup->translateOrNew('sk')->name = 'Všeobecné';
+        $generalTagGroup->translateOrNew('de')->name = 'Generell';
+        $generalTagGroup->save();
+
         $testTag1 = Tag::firstOrNew(['identifier' => 'TEST_TAG1']);
-        $testTag1->name = 'Test tag 1';
+        $testTag1->name = 'Testing tag 1';
+        $testTag1->translateOrNew('de')->name = 'Tag zum testen 1';
         $testTag1->tagGroup()->associate($generalTagGroup);
         $testTag1->save();
 
         $testTag2 = Tag::firstOrNew(['identifier' => 'TEST_TAG2']);
-        $testTag2->name = 'Test tag 2';
+        $testTag2->name = 'Testing tag 2';
+        $testTag2->translateOrNew('sk')->name = 'Testovací tag 2';
         $testTag2->tagGroup()->associate($generalTagGroup);
         $testTag2->save();
-
-
     }
 }
