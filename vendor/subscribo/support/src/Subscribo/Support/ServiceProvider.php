db47ab3f749e37bbeb1c6e97aea7fe6d46747c20
slavo <slavo@localhost>
2015-04-09 11:28:08 +0200

    Package Subscribo Support update  - refactoring and updating ServiceProvider:    - fixes in Fixes/ServiceProvider    - enhancements (previous and new) in EnhancedServiceProviderTrait  - composer.json update

--- vendor/subscribo/support/src/Subscribo/Support/ServiceProvider.php
+++ vendor/subscribo/support/src/Subscribo/Support/ServiceProvider.php
@@ -1,43 +1,15 @@
 <?php namespace Subscribo\Support;
 
-use ReflectionObject;
+use Subscribo\Support\Traits\EnhancedServiceProviderTrait;
 
 /**
  * Class ServiceProvider
- * Extending Laravel Framework class and providing some additional functionality
+ * Extending Laravel Framework class fixing and providing some additional functionality
  *
  * @package Subscribo\Support
  */
-abstract class ServiceProvider extends \Illuminate\Support\ServiceProvider
+abstract class ServiceProvider extends \Subscribo\Support\Fixes\ServiceProvider
 {
-    /**
-     * Overload if child class is in non-standard directory
-     * @return string
-     */
-    protected function getPackagePath()
-    {
-        $reflection = new ReflectionObject($this);
-        $fileName = $reflection->getFileName();
-        $packagePath = dirname(dirname(dirname(dirname(dirname(dirname($fileName))))));
-        return $packagePath;
-    }
+    use EnhancedServiceProviderTrait;
 
-    /**
-     * @param string|array $resources
-     * @param string|bool $namespace
-     */
-    protected function registerTranslationResources($resources, $namespace = true)
-    {
-        $packagePath = $this->getPackagePath();
-        if (true === $namespace) {
-            $namespace = basename($packagePath);
-        }
-        $basePath = $this->app->make('path.base');
-        $packageTranslationsPath = $this->getPackagePath().'/resources/lang/';
-        $applicationTranslationPath = $basePath.'/subscribo/resources/lang/'.$namespace.'/';
-        $this->publishes([$packageTranslationsPath => $applicationTranslationPath], 'translation');
-
-        $manager = $this->app->make('\\Subscribo\\Localization\\Interfaces\\LocalizationResourcesManagerInterface');
-        $manager->registerNamespace($namespace, [$packageTranslationsPath, $applicationTranslationPath], $resources);
-    }
 }
