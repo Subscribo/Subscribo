7341928f0ab16e6dae04daf9728155523f2b6daa
slavo <slavo@localhost>
2015-04-09 15:46:24 +0200

    Package Subscribo ApiClientAuth update  - refactoring of ApiClientAuthServiceProvider

--- vendor/subscribo/apiclientauth/src/Subscribo/ApiClientAuth/Integration/Laravel/ApiClientAuthServiceProvider.php
+++ vendor/subscribo/apiclientauth/src/Subscribo/ApiClientAuth/Integration/Laravel/ApiClientAuthServiceProvider.php
@@ -15,28 +15,44 @@ class ApiClientAuthServiceProvider extends ServiceProvider
     /** @var bool  */
     protected $routesRegistered = false;
 
+
     public function register()
     {
-        $this->app->register('\\Subscribo\\Localization\\Integration\\Laravel\\LocalizationServiceProvider');
-        $this->forRouteRegistration[] = $this->app->register('Subscribo\\ApiClientCommon\\Integration\\Laravel\\ApiClientCommonServiceProvider');
+        $this->registerDependencies();
     }
+
 
     public function boot()
     {
-        $this->app->make('auth')->extend('remote', function ($app) {
-            return $app->make('Subscribo\\ApiClientAuth\\RemoteAccountProvider');
-        });
+        $this->extendAuthWithRemoteDriver();
+
         $this->registerTranslationResources('messages');
     }
 
-    public function registerRoutes($router, array $middleware, array $paths = array())
+
+    public function registerRoutes(array $middleware, array $paths = array(), $router = null)
     {
         if ($this->routesRegistered) {
             return;
         }
         foreach ($this->forRouteRegistration as $serviceProvider) {
-            $serviceProvider->registerRoutes($router, $middleware, $paths);
+            $serviceProvider->registerRoutes($middleware, $paths, $router);
         }
         $this->routesRegistered = true;
+    }
+
+
+    public function extendAuthWithRemoteDriver()
+    {
+        $this->app->make('auth')->extend('remote', function ($app) {
+            return $app->make('Subscribo\\ApiClientAuth\\RemoteAccountProvider');
+        });
+    }
+
+
+    protected function registerDependencies()
+    {
+        $this->app->register('\\Subscribo\\Localization\\Integration\\Laravel\\LocalizationServiceProvider');
+        $this->forRouteRegistration[] = $this->registerServiceProvider('Subscribo\\ApiClientCommon\\Integration\\Laravel\\ApiClientCommonServiceProvider');
     }
 }
