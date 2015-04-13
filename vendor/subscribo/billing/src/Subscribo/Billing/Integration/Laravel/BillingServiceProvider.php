256b6a28af8fb44ce3820f63db62252497075c3c
slavo <slavo@localhost>
2015-04-13 17:53:35 +0200

    Package Subscribo Billing initial commit

---
+++ vendor/subscribo/billing/src/Subscribo/Billing/Integration/Laravel/BillingServiceProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Subscribo\Billing\Integration\Laravel;
+
+use Subscribo\Support\ServiceProvider;
+
+/**
+ * Class BillingServiceProvider
+ *
+ * @package Subscribo\Billing
+ */
+class BillingServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
