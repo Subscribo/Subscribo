08c1f6c6215973b0eea02c18059dd011a2c0fc08
slavo <slavo@localhost>
2015-04-13 15:44:41 +0200

    Symfony Packages update

--- vendor/symfony/config/Symfony/Component/Config/Loader/FileLoader.php
+++ vendor/symfony/config/Symfony/Component/Config/Loader/FileLoader.php
@@ -82,7 +82,7 @@ abstract class FileLoader extends Loader
         try {
             $loader = $this->resolve($resource, $type);
 
-            if ($loader instanceof FileLoader && null !== $this->currentDir) {
+            if ($loader instanceof self && null !== $this->currentDir) {
                 // we fallback to the current locator to keep BC
                 // as some some loaders do not call the parent __construct()
                 // @deprecated should be removed in 3.0
@@ -93,7 +93,7 @@ abstract class FileLoader extends Loader
             $resources = is_array($resource) ? $resource : array($resource);
             for ($i = 0; $i < $resourcesCount = count($resources); $i++) {
                 if (isset(self::$loading[$resources[$i]])) {
-                    if ($i == $resourcesCount-1) {
+                    if ($i == $resourcesCount - 1) {
                         throw new FileLoaderImportCircularReferenceException(array_keys(self::$loading));
                     }
                 } else {
--- vendor/symfony/http-kernel/Symfony/Component/HttpKernel/Kernel.php
+++ vendor/symfony/http-kernel/Symfony/Component/HttpKernel/Kernel.php
@@ -60,11 +60,11 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '2.6.6-DEV';
-    const VERSION_ID = '20606';
+    const VERSION = '2.6.7-DEV';
+    const VERSION_ID = '20607';
     const MAJOR_VERSION = '2';
     const MINOR_VERSION = '6';
-    const RELEASE_VERSION = '6';
+    const RELEASE_VERSION = '7';
     const EXTRA_VERSION = 'DEV';
 
     /**
