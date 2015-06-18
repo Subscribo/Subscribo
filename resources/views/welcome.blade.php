fd0edbc06e96f55b48469e219f6ee08bab192308
slavo <slavo2@users.noreply.github.com>
2015-06-18 19:04:11 +0200

    resources and storage directory update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,58 +9,58 @@
 /** @var \Subscribo\Localizer $localizer */
 $localizer = \Subscribo\Localization::localizer('app', 'main');
 ?>
+<!DOCTYPE html>
 <html lang="{{ $localizer->getBCPLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>
             @section('pageTitle')
             {{ $localizer->trans('page.title') }}
             @show
         </title>
+        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+        <style>
+            html, body {
+                height: 100%;
+            }
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
 
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
+            .title {
+                font-size: 96px;
+                margin-bottom: 40px;
+            }
 
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">{{ $localizer->trans('welcome.title') }}</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
+            .quote {
+                font-size: 24px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">{{ $localizer->trans('welcome.title') }}</div>
+                <div class="quote">{{ Inspiring::quote() }}</div>
+            </div>
+        </div>
+    </body>
 </html>
