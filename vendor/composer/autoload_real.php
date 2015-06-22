1240c866d11c3fc43ec5d3e58503a328c7c570b4
slavo <slavo2@users.noreply.github.com>
2015-06-22 12:22:35 +0200

    composer files and composer.lock update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInit3123cee2938a8db3de847ec35e600333
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit3123cee2938a8db3de847ec35e600333', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
