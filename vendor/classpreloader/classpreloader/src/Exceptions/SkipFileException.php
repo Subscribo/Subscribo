d8533692bf239e62d39c0780d02bcab6b9b55c6a
slavo <slavo2@users.noreply.github.com>
2015-07-15 14:01:48 +0200

    Package classpreloader update, removal of bin/classpreloader.php

--- vendor/classpreloader/classpreloader/src/Exceptions/SkipFileException.php
+++ vendor/classpreloader/classpreloader/src/Exceptions/SkipFileException.php
@@ -3,14 +3,14 @@
 /*
  * This file is part of Class Preloader.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  * (c) Michael Dowling <mtdowling@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace ClassPreloader\Exception;
+namespace ClassPreloader\Exceptions;
 
 use Exception;
 
