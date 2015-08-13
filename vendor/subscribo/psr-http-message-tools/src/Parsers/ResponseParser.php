9742b0a4624b149b7c2eb580740f4695ee869424
slavo <slavo2@users.noreply.github.com>
2015-08-13 17:49:24 +0200

    Package Subscribo PsrHttpMessageTools - ResponseParser - removal of alternative parsing of application/x-www-form-urlencoded

--- vendor/subscribo/psr-http-message-tools/src/Parsers/ResponseParser.php
+++ vendor/subscribo/psr-http-message-tools/src/Parsers/ResponseParser.php
@@ -22,58 +22,6 @@ class ResponseParser implements DealingWithContentTypeInterface
     }
 
     /**
-     * Parses data of Content-Type application/x-www-form-urlencoded into an array
-     * @param string $data
-     * @return array
-     */
-    public static function parseContentTypeApplicationXWwwFormUrlencoded($data)
-    {
-        $result = [];
-        $parts = explode('&', $data);
-        foreach ($parts as $part) {
-            $elements = explode('=', $part, 2);
-            $key = urldecode($elements[0]);
-            if (empty($key)) {
-                continue;
-            }
-            $value = isset($elements[1]) ? urldecode($elements[1]) : null;
-            if (isset($result[$key])) {
-                $result[$key] = (array) ($result[$key]);
-                $result[$key][] = $value;
-            } else {
-                $result[$key] = $value;
-            }
-        }
-        return $result;
-    }
-
-    /**
-     * Manually process that kind of transformation of one-dimensional array into multidimensional
-     * which PHP's parse_str() does
-     *
-     * @param array $source
-     * @return array
-     */
-    public static function transformBracketedArrayIntoMultidimensional(array $source)
-    {
-        $result = [];
-        foreach ($source as $key => $value) {
-            $keyParts = explode('[', $key);
-            $keyPartsReversed = array_reverse($keyParts);
-            $toAdd = $value;
-            foreach ($keyPartsReversed as $keyPart) {
-                $keyCleared = trim($keyPart, '[]');
-                if ($keyCleared) {
-                    $toAdd = [$keyCleared => $toAdd];
-                }
-            }
-            $toAdd = (array) $toAdd;
-            $result = array_replace_recursive($result, $toAdd);
-        }
-        return $result;
-    }
-
-    /**
      * @param ResponseInterface $response
      * @return array
      */
@@ -118,7 +66,9 @@ class ResponseParser implements DealingWithContentTypeInterface
         $format = strtolower($format);
         switch ($format) {
             case static::CONTENT_TYPE_FORM:
-                return static::parseContentTypeApplicationXWwwFormUrlencoded($value);
+                $data = [];
+                parse_str($value, $data);
+                return $data;
             case static::CONTENT_TYPE_JSON:
                 $data = json_decode($value, true, 512, JSON_BIGINT_AS_STRING);
                 $data = is_array($data) ? $data : [];
