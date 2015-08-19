1546bc756c7310073e7cc554fb6e555a2b3248d9
slavo <slavo2@users.noreply.github.com>
2015-08-19 15:18:00 +0200

    Package Subscribo DevelopmentSeeder update  - TransactionGatewaySeeder update  - composer.json update

--- vendor/subscribo/developmentseeder/src/Subscribo/DevelopmentSeeder/Seeds/TransactionGatewaySeeder.php
+++ vendor/subscribo/developmentseeder/src/Subscribo/DevelopmentSeeder/Seeds/TransactionGatewaySeeder.php
@@ -4,6 +4,8 @@ namespace Subscribo\DevelopmentSeeder\Seeds;
 
 use Illuminate\Database\Seeder;
 use Subscribo\ModelCore\Models\TransactionGateway;
+use Subscribo\TransactionPluginKlarna\Drivers\InvoiceDriver;
+use Subscribo\TransactionPluginPayUnity\Drivers\CopyAndPayDriver;
 
 class TransactionGatewaySeeder extends Seeder
 {
@@ -13,12 +15,12 @@ class TransactionGatewaySeeder extends Seeder
         $klarnaInvoice = TransactionGateway::firstOrCreate(['identifier' => 'KLARNA-INVOICE']);
 
         $payUnityCopyAndPay->name = 'PayUnity';
-        $payUnityCopyAndPay->gateway = 'PayUnity\\COPYAndPAY';
+        $payUnityCopyAndPay->driver = CopyAndPayDriver::getDriverIdentifier();
         $payUnityCopyAndPay->description = 'For Credit / Debit card payments, SOFORT UEBERWEISUNG...';
         $payUnityCopyAndPay->save();
 
         $klarnaInvoice->name = 'Klarna Invoice';
-        $klarnaInvoice->gateway = 'Klarna\\Invoice';
+        $klarnaInvoice->driver = InvoiceDriver::getDriverIdentifier();
         $klarnaInvoice->description = 'To be invoiced by Klarna';
         $klarnaInvoice->save();
     }
