dba9531b178a406e2e1c49939631f73634fcd74f
slavo <slavo2@users.noreply.github.com>
2015-09-08 14:44:15 +0200

    Package Subscribo ApiServeJobs update  - new job MaintainSubscription  - job ServiceMaintenance update    - generating and dispatching MaintainSubscription jobs    - log level changed to info  - job MaintainDeliveries update:     - some log level changed to info

--- vendor/subscribo/apiserverjob/src/Subscribo/ApiServerJob/Jobs/Regular/MaintainDeliveries.php
+++ vendor/subscribo/apiserverjob/src/Subscribo/ApiServerJob/Jobs/Regular/MaintainDeliveries.php
@@ -32,14 +32,14 @@ class MaintainDeliveries extends AbstractJob
      */
     public function handle(LoggerInterface $log)
     {
-        $log->notice("Maintaining Deliveries for service '".$this->service->identifier."' started");
+        $log->info("Maintaining Deliveries for service '".$this->service->identifier."' started");
 
         $addedDeliveries = Delivery::autoAdd($this->service);
 
         foreach ($addedDeliveries as $delivery) {
             $log->notice('Added delivery starting from '.DateTimeUtils::exportDateTime($delivery->start));
         }
-        $log->notice("Deliveries added: ". count($addedDeliveries));
+        $log->info("Deliveries added: ". count($addedDeliveries));
 
         $updated = Delivery::autoAvailable($this->service);
 
@@ -49,8 +49,8 @@ class MaintainDeliveries extends AbstractJob
             .", stayed enabled: ".count($updated['stayedEnabled'])
             .", stayed disabled: ".count($updated['stayedDisabled'])
         );
-        $log->notice("Maintaining Deliveries for service '".$this->service->identifier."' finished");
-        $log->notice("Maintaining Product Realizations for service '".$this->service->identifier."' started");
+        $log->info("Maintaining Deliveries for service '".$this->service->identifier."' finished");
+        $log->info("Maintaining Product Realizations for service '".$this->service->identifier."' started");
 
         $products = $this->service->products;
 
@@ -71,8 +71,8 @@ class MaintainDeliveries extends AbstractJob
                     ."' for delivery starting from ".DateTimeUtils::exportDateTime($realization->delivery->start)
                 );
         }
-        $log->notice("Realizations supplied: ".count($suppliedRealizations));
-        $log->notice("Maintaining Product Realizations for service '".$this->service->identifier."' finished");
+        $log->info("Realizations supplied: ".count($suppliedRealizations));
+        $log->info("Maintaining Product Realizations for service '".$this->service->identifier."' finished");
     }
 }
 
--- vendor/subscribo/apiserverjob/src/Subscribo/ApiServerJob/Jobs/Regular/ServiceMaintenance.php
+++ vendor/subscribo/apiserverjob/src/Subscribo/ApiServerJob/Jobs/Regular/ServiceMaintenance.php
@@ -4,6 +4,7 @@ namespace Subscribo\ApiServerJob\Jobs\Regular;
 
 use Subscribo\ApiServerJob\Jobs\AbstractJob;
 use Subscribo\ApiServerJob\Jobs\Regular\MaintainDeliveries;
+use Subscribo\ApiServerJob\Jobs\Regular\MaintainSubscription;
 use Subscribo\ModelCore\Models\Service;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Psr\Log\LoggerInterface;
@@ -33,12 +34,17 @@ class ServiceMaintenance extends AbstractJob
      */
     public function handle(LoggerInterface $log)
     {
-        $log->notice("Maintenance of service '".$this->service->identifier."' (dispatching regular jobs) started.");
+        $log->info("Maintenance of service '".$this->service->identifier."' (dispatching regular jobs) started.");
 
         $maintainDeliveriesJob = new MaintainDeliveries($this->service);
         $this->dispatch($maintainDeliveriesJob);
 
-        $log->notice("Deliveries maintenance for service '".$this->service->identifier."' dispatched.");
-        $log->notice("Maintenance of service '".$this->service->identifier."' (dispatching regular jobs) finished.");
+        $log->info("Deliveries maintenance for service '".$this->service->identifier."' dispatched.");
+        foreach ($this->service->subscriptions as $subscription) {
+            $maintainSubscriptionJob = new MaintainSubscription($subscription);
+            $this->dispatch($maintainSubscriptionJob);
+            $log->info("Subscription ID '".$subscription->id."' maintenance dispatched.");
+        }
+        $log->info("Maintenance of service '".$this->service->identifier."' (dispatching regular jobs) finished.");
     }
 }
