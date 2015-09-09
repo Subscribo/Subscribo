55c07e73203985cced65de7c51974d2996d63f3f
slavo <slavo2@users.noreply.github.com>
2015-09-09 17:02:44 +0200

    Zeitmaschine :) (developer tool - to make it a runnable script you need to run e.g. chmod +x bin/zeitmaschine.sh )

---
+++ bin/zeitmaschine.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$*" = off ]
+then
+sudo service vboxadd-service start
+date
+elif [ -z "$*" ]
+then
+sudo service vboxadd-service stop
+date
+sudo date -s "+ 7 days"
+else
+sudo service vboxadd-service stop
+date
+sudo date -s "$*"
+fi
+echo Starting queue listener
+php artisan queue:listen > /dev/null 2>&1 &
+
