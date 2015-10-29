86f9f1f2d8ae525f4c9c3fb5595f2a53704ae39f
slavo <slavo2@users.noreply.github.com>
2015-10-29 17:03:05 +0100

    New package Subscribo ClientIntegrationCommon

---
+++ vendor/subscribo/client-integration-common/src/Subscribo/ClientIntegrationCommon/Interfaces/ClientIntegrationInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Subscribo\ClientIntegrationCommon\Interfaces;
+
+/**
+ * Interface ClientIntegrationInterface
+ *
+ * @package Subscribo\ClientIntegrationCommon
+ */
+interface ClientIntegrationInterface
+{
+    /**
+     * @return int|null
+     */
+    public function getAccountId();
+}
