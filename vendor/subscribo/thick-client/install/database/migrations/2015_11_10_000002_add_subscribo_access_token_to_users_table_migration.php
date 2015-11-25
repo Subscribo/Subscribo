4c9e95119423639c95e75fe1ff9ecef474e3e421
slavo <slavo2@users.noreply.github.com>
2015-11-25 23:34:54 +0100

    Package Subscribo ThickClient update  - fix in 2015_11_10_add_subscribo_access_token_to_users_table_migration.php

--- vendor/subscribo/thick-client/install/database/migrations/2015_11_10_000002_add_subscribo_access_token_to_users_table_migration.php
+++ vendor/subscribo/thick-client/install/database/migrations/2015_11_10_000002_add_subscribo_access_token_to_users_table_migration.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
 
 class AddSubscriboAccessTokenToUsersTableMigration extends Migration
 {
