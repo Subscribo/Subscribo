adc865923a88a5df762b209d642c5e03673c7dc7
slavo <slavo2@users.noreply.github.com>
2016-05-18 00:28:04 +0200

    Package Subscribo integration-to-spark  - fix and update of view plugin.blade.php

--- vendor/subscribo/integration-to-spark/resources/views/integration-to-spark/plugin.blade.php
+++ vendor/subscribo/integration-to-spark/resources/views/integration-to-spark/plugin.blade.php
@@ -1,1 +1,5 @@
-<iframe src="/plugin/checkout/products" style="width: 100%; height:100vh" width="100%" height="100%" frameborder="0" seamless="seamless"></iframe>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <iframe src="/subscribo/plugin/checkout/products" style="width: 100%; height:100vh" width="100%" height="100%" frameborder="0" seamless="seamless"></iframe>
+    </div>
+</div>
